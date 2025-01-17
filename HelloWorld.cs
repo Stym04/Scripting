6ce96abaa078cc2fa00246fdca98cbf05fdb2a9f
Stym04 <marcosmarsol64@gmail.com>
2025-01-17 20:29:44 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Aqui se saluda :D
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
